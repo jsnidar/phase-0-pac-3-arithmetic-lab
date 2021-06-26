e78df328a7fb02e3d470e3d2ca3cd245ecbbedfc
Jeremiah <jsnidar@gmail.com>
2021-06-26 10:03:07 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+// add function
+function add(num1, num2) {
+    return num1 + num2
+}
+// subtract function
+function subtract(num1, num2) {
+    return num1 - num2
+}
+// multiply function
+function multiply(num1, num2) {
+    return num1 * num2
+}
+// divide function
+function divide(num1, num2) {
+    return num1/num2
+}
+
+function increment(n) {
+    n++
+    return n
+}
+
+function decrement(n) {
+    n--
+    return n
+}
+
+function makeInt(string) {
+   let n = parseInt(string, 10)
+    return n
+}
+
+function preserveDecimal(string) {
+   let n = parseFloat(string)
+    return n
+}
